5ce76d6f3937e753d18e59feaba35db45e8d973b
mritsurgeon <59644778+mritsurgeon@users.noreply.github.com>
2023-02-18 15:26:53 +0200

    Create open_ports.sh

---
+++ Deployment/open_ports.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rg_name=$1
+vm_name=$2
+
+# Open the required ports
+az vm open-port \
+    --name $vm_name \
+    --resource-group $rg_name \
+    --port 80 \
+    --priority 1001
+
+az vm open-port \
+    --name $vm_name \
+    --resource-group $rg_name \
+    --port 443 \
+    --priority 1002
+
+# Print a message for the user
+echo "Ports opened successfully!"
