5ad504e5a57b5dd963ad0373b6c3d4ab63d73d7a
mritsurgeon <59644778+mritsurgeon@users.noreply.github.com>
2023-02-28 17:02:43 +0200

    Update deploy_saas.sh
    Changed Deploy PS1 param

--- Deployment/deploy_saas.sh
+++ Deployment/deploy_saas.sh
@@ -17,14 +17,7 @@ Location=$8
 
 
 # Call the Deploy.ps1 script
-pwsh ./Commercial-Marketplace-SaaS-Accelerator/deployment/Deploy.ps1 `
-  -WebAppNamePrefix "$WebAppNamePrefix" `
-  -SQLServerName "$SQLServerName" `
-  -SQLAdminLogin "$SQLAdminLogin" `
-  -SQLAdminLoginPassword "$SQLAdminLoginPassword" `
-  -PublisherAdminUsers "$PublisherAdminUsers" `
-  -ResourceGroupForDeployment "$ResourceGroupForDeployment" `
-  -Location "$Location"
+pwsh ./Commercial-Marketplace-SaaS-Accelerator/deployment/Deploy.ps1 -WebAppNamePrefix $WebAppNamePrefix -SQLServerName $SQLServerName -SQLAdminLogin $SQLAdminLogin -SQLAdminLoginPassword '$SQLAdminLoginPassword' -PublisherAdminUsers $PublisherAdminUsers -ResourceGroupForDeployment $ResourceGroupForDeployment -Location $Location
 
 # Print a message for the user
 echo "SaaS accelerator deployed successfully!!"
