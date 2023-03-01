8f7b5a030622adcc98d04261b789882e95d8be1d
mritsurgeon <59644778+mritsurgeon@users.noreply.github.com>
2023-03-01 08:51:18 +0200

    Update deploy_saas.sh
    Removed SQL Admin Passowords

--- Deployment/deploy_saas.sh
+++ Deployment/deploy_saas.sh
@@ -10,14 +10,12 @@ git clone $saas_repo
 # Define the required parameters
 WebAppNamePrefix=$3
 SQLServerName=$4
-SQLAdminLogin=$5
-SQLAdminLoginPassword=$6
-PublisherAdminUsers=$7
-Location=$8
+PublisherAdminUsers=$5
+Location=$6
 
 
 # Call the Deploy.ps1 script
-pwsh ./Commercial-Marketplace-SaaS-Accelerator/deployment/Deploy.ps1 -WebAppNamePrefix $WebAppNamePrefix -SQLServerName $SQLServerName -SQLAdminLogin $SQLAdminLogin -SQLAdminLoginPassword '$SQLAdminLoginPassword' -PublisherAdminUsers $PublisherAdminUsers -ResourceGroupForDeployment $ResourceGroupForDeployment -Location $Location
+pwsh ./Commercial-Marketplace-SaaS-Accelerator/deployment/Deploy.ps1 -WebAppNamePrefix $WebAppNamePrefix -SQLServerName $SQLServerName -PublisherAdminUsers $PublisherAdminUsers -ResourceGroupForDeployment $ResourceGroupForDeployment -Location $Location
 
 # Print a message for the user
 echo "SaaS accelerator deployed successfully!!"
