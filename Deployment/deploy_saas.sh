db305b3b6567c70ed4f7556ccf0fe266706f4b2d
mritsurgeon <59644778+mritsurgeon@users.noreply.github.com>
2023-03-02 15:16:55 +0200

    Update deploy_saas.sh
    added location for Deployment

--- Deployment/deploy_saas.sh
+++ Deployment/deploy_saas.sh
@@ -11,10 +11,11 @@ git clone $saas_repo
 WebAppNamePrefix=$3
 SQLServerName=$4
 PublisherAdminUsers=$5
+location=$6
 
 
 # Call the Deploy.ps1 script
-pwsh ./Commercial-Marketplace-SaaS-Accelerator/deployment/Deploy.ps1 -WebAppNamePrefix $WebAppNamePrefix -SQLServerName $SQLServerName -PublisherAdminUsers $PublisherAdminUsers -ResourceGroupForDeployment $ResourceGroupForDeployment
+pwsh ./Commercial-Marketplace-SaaS-Accelerator/deployment/Deploy.ps1 -WebAppNamePrefix $WebAppNamePrefix -SQLServerName $SQLServerName -PublisherAdminUsers $PublisherAdminUsers -ResourceGroupForDeployment $ResourceGroupForDeployment $location
 
 # Print a message for the user
 echo "SaaS accelerator deployed successfully!!"
