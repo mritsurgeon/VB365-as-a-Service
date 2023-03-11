b3589db4ad0f3a1dc21d7a48c43b9709b11ec37c
mritsurgeon <59644778+mritsurgeon@users.noreply.github.com>
2023-03-11 11:55:30 +0200

    Update deploy_workflow.sh
    Updated AZ Create Parameters

--- Deployment/deploy_workflow.sh
+++ Deployment/deploy_workflow.sh
@@ -7,17 +7,19 @@ subnet_name=$3
 logic_app_name=$4
 location=$5
 
+
+
 # Get the subnet ID
 subnet_id=$(az network vnet subnet show --resource-group $rg_name --vnet-name $vnet_name --name $subnet_name --query id --output tsv)
 
 # Create the Logic App
-az logic workflow create --resource-group $rg_name --name $logic_app_name --location $location
+az logic workflow create --resource-group $rg_name --location $location --name $logic_app_name --definition "../Project Amafu/LogicApp/Mail-Trigger/workflow.json"
 
 # Update the Logic App to use the correct subnet
 az logic workflow update --resource-group $rg_name --name $logic_app_name --subnet $subnet_id
 
 # Deploy the Logic App workflow
-az logic workflow import --resource-group $rg_name --name $logic_app_name --input-file "../Project Amafu/LogicApp/Mail-Trigger/workflow.json"
+#az logic workflow import --resource-group $rg_name --name $logic_app_name --input-file "../Project Amafu/LogicApp/Mail-Trigger/workflow.json"
 
 # Print confirmation message
 echo "Logic App deployed successfully!!"
