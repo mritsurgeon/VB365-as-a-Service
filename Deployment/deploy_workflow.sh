2ac641929ba927b9b5f8ac4f571722e7eda5f0ed
mritsurgeon <59644778+mritsurgeon@users.noreply.github.com>
2023-03-12 10:08:03 +0200

    Update deploy_workflow.sh

--- Deployment/deploy_workflow.sh
+++ Deployment/deploy_workflow.sh
@@ -9,15 +9,6 @@ location=$5
 subscriptionId=$(az account show --query id | tr -d '"')
 app_plan="asp-${logic_app_name}-plan" 
 
-# Get the subnet ID
-#subnet_id=$(az network vnet subnet show --resource-group $rg_name --vnet-name $vnet_name --name $subnet_name --query id --output tsv)
-# Create the Logic App
-#az logic workflow create --resource-group $rg_name --location $location --name $logic_app_name --definition "../Project Amafu/LogicApp/Mail-Trigger/workflow.json"
-# Update the Logic App to use the correct subnet
-#az logic workflow update --resource-group $rg_name --name $logic_app_name --subnet $subnet_id
-# Deploy the Logic App workflow
-#az logic workflow import --resource-group $rg_name --name $logic_app_name --input-file "../Project Amafu/LogicApp/Mail-Trigger/workflow.json"
-
 az appservice plan create --name $app_plan --resource-group $rg_name --location $location --sku "WS1" 
 
 az deployment group create --resource-group $rg_name --template-uri https://raw.githubusercontent.com/mritsurgeon/VB365-as-a-Service/main/Deployment/templates/logic.json --parameters name=$logic_app_name location=$location serverFarmResourceGroup=$rg_name subnetName=api vnetName=$vnet_name ResourceGroup=$rg_name subscriptionId=$subscriptionId hostingPlanName=$app_plan
