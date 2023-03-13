524947a9743b0a07c3c033b74733886789e1bc3e
mritsurgeon <59644778+mritsurgeon@users.noreply.github.com>
2023-03-13 20:10:06 +0200

    Update deploy_workflow.sh

--- Deployment/deploy_workflow.sh
+++ Deployment/deploy_workflow.sh
@@ -10,10 +10,18 @@ subscriptionId=$(az account show --query id | tr -d '"')
 app_plan="asp-${logic_app_name}-plan" 
 storageAccountName="web${logic_app_name}storage"
 
-
+# Create a Service Plan to host logic app
 az appservice plan create --name $app_plan --resource-group $rg_name --location $location --sku "WS1" 
 
+# Deploy Logic app from ARM with required settings
 az deployment group create --resource-group $rg_name --template-uri https://raw.githubusercontent.com/mritsurgeon/VB365-as-a-Service/main/Deployment/templates/logic.json --parameters name=$logic_app_name location=$location serverFarmResourceGroup=$rg_name subnetName=api vnetName=$vnet_name ResourceGroup=$rg_name subscriptionId=$subscriptionId hostingPlanName=$app_plan storageAccountName=$storageAccountName
+
+# Change directory 
+cd "../Project Amafu/"
+
+# Deploy worflow & parameters
+az logicapp deployment source config-zip --name $logic_app_name --resource-group $rg_name --src LogicApp.zip
+
 
 # Print confirmation message
 echo "Logic App deployed successfully!!"
