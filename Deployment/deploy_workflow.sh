9f5371af8134fa913d65f777343daa7767de5fab
mritsurgeon <59644778+mritsurgeon@users.noreply.github.com>
2023-03-13 19:05:22 +0200

    Update deploy_workflow.sh
    avoid storage account conflicts

--- Deployment/deploy_workflow.sh
+++ Deployment/deploy_workflow.sh
@@ -8,10 +8,12 @@ logic_app_name=$4
 location=$5
 subscriptionId=$(az account show --query id | tr -d '"')
 app_plan="asp-${logic_app_name}-plan" 
+storageAccountName="web${logic_app_name}storage"
+
 
 az appservice plan create --name $app_plan --resource-group $rg_name --location $location --sku "WS1" 
 
-az deployment group create --resource-group $rg_name --template-uri https://raw.githubusercontent.com/mritsurgeon/VB365-as-a-Service/main/Deployment/templates/logic.json --parameters name=$logic_app_name location=$location serverFarmResourceGroup=$rg_name subnetName=api vnetName=$vnet_name ResourceGroup=$rg_name subscriptionId=$subscriptionId hostingPlanName=$app_plan
+az deployment group create --resource-group $rg_name --template-uri https://raw.githubusercontent.com/mritsurgeon/VB365-as-a-Service/main/Deployment/templates/logic.json --parameters name=$logic_app_name location=$location serverFarmResourceGroup=$rg_name subnetName=api vnetName=$vnet_name ResourceGroup=$rg_name subscriptionId=$subscriptionId hostingPlanName=$app_plan storageAccountName=$storageAccountName
 
 # Print confirmation message
 echo "Logic App deployed successfully!!"
